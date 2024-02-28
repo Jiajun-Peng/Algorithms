7349fda68db85750a9a795ceee3f22ae04c328c1
Jiajun-Peng <pengjiajun566@gmail.com>
2024-02-28 23:04:45 +0800

    暴力递归的动态规划方式优化-机器人运动问题

--- src/dp/RobotToTarget.java
+++ src/dp/RobotToTarget.java
@@ -22,18 +22,18 @@ package dp;
  */
 public class RobotToTarget {
     // 暴力递归实现
-    public static int f(int n, int target, int cur, int restStep) {
+    public static int f(int n, int target, int start, int restStep) {
         if (restStep == 0) {
-           return cur == target ? 1 : 0; // 无剩余步数，当前位置等于目标位置时，说明当前方法有效，返回1否则0
+           return start == target ? 1 : 0; // 无剩余步数，当前位置等于目标位置时，说明当前方法有效，返回1否则0
         }
-        if (cur == 1) {
-            return f(n, target, cur + 1, restStep - 1); // 当来到最左边时，只能向右走
+        if (start == 1) {
+            return f(n, target, start + 1, restStep - 1); // 当来到最左边时，只能向右走
         }
-        if (cur == n) {
-            return f(n, target, cur - 1, restStep - 1); // 当来到最右边时，只能向左走
+        if (start == n) {
+            return f(n, target, start - 1, restStep - 1); // 当来到最右边时，只能向左走
         }
-        return f(n, target, cur - 1, restStep - 1)
-                + f(n, target, cur + 1, restStep - 1); // 来到中间时，可往左走也可往右走
+        return f(n, target, start - 1, restStep - 1)
+                + f(n, target, start + 1, restStep - 1); // 来到中间时，可往左走也可往右走
     }
 
     /**
@@ -41,38 +41,67 @@ public class RobotToTarget {
      * cur的范围可以是 1~n，restStep的范围是 0~restStep
      * 所以可以创建一个int[n][restStep]的二维数组作为缓存
      */
-    public static int f_cache(int n, int target, int cur, int restStep) {
+    public static int f_cache(int n, int target, int start, int restStep) {
+        if (n < 2 || target < 1 || target > n || start < 1 || start > n || restStep < 1) {
+            return 0;
+        }
         int[][] cache = new int[n + 1][restStep + 1];
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= restStep; j++) {
                 cache[i][j] = -1;
             }
         }
-        return f_cache(n, target, cur, restStep, cache);
+        return f_cache(n, target, start, restStep, cache);
     }
-    public static int f_cache(int n, int target, int cur, int restStep, int[][] cache) {
-        if (cache[cur][restStep] != -1) {
-            return cache[cur][restStep];
+    public static int f_cache(int n, int target, int start, int restStep, int[][] cache) {
+        if (cache[start][restStep] != -1) {
+            return cache[start][restStep];
         }
         int result;
         if (restStep == 0) {
-            result = cur == target ? 1 : 0; // 无剩余步数，当前位置等于目标位置时，说明当前方法有效，返回1否则0
-        } else if (cur == 1) {
-            result = f_cache(n, target, cur + 1, restStep - 1, cache); // 当来到最左边时，只能向右走
-        } else if (cur == n) {
-            result =  f_cache(n, target, cur - 1, restStep - 1, cache); // 当来到最右边时，只能向左走
+            result = start == target ? 1 : 0; // 无剩余步数，当前位置等于目标位置时，说明当前方法有效，返回1否则0
+        } else if (start == 1) {
+            result = f_cache(n, target, start + 1, restStep - 1, cache); // 当来到最左边时，只能向右走
+        } else if (start == n) {
+            result =  f_cache(n, target, start - 1, restStep - 1, cache); // 当来到最右边时，只能向左走
         } else {
-            result = f_cache(n, target, cur - 1, restStep - 1, cache)
-                    + f_cache(n, target, cur + 1, restStep - 1, cache); // 来到中间时，可往左走也可往右走
+            result = f_cache(n, target, start - 1, restStep - 1, cache)
+                    + f_cache(n, target, start + 1, restStep - 1, cache); // 来到中间时，可往左走也可往右走
         }
-        cache[cur][restStep] = result;
+        cache[start][restStep] = result;
         return result;
+    }
+
+    /*
+    因为最终的结果依赖于start和restStep的组合参数，这就是一个二维表,可以将start作为纵坐标、restStep作为横坐标
+
+     */
+    public static int f_dp(int n, int target, int start, int restStep) {
+        if (n < 2 || target < 1 || target > n || start < 1 || start > n || restStep < 1) {
+            return 0;
+        }
+        int[][] dp = new int[n + 1][restStep + 1]; // start的取值范围是 1~n
+
+        // 根据尝试暴力递归过程，我们知道，当restStep == 0时，
+        dp[target][0] = 1;
+
+        // 依赖的过程是从左向右、从上到下
+        for (int restIndex = 1; restIndex <= restStep; restIndex++) {
+            dp[1][restIndex] = dp[2][restIndex - 1];
+            for (int j = 2; j < n; j++) {
+                dp[j][restIndex] = dp[j - 1][restIndex - 1] + dp[j + 1][restIndex - 1];
+            }
+            dp[n][restIndex] = dp[n - 1][restIndex - 1];
+        }
+        return dp[start][restStep];
     }
 
     public static void main(String[] args) {
         int f = f(10, 5, 3, 6);
         int cache = f_cache(10, 5, 3, 6);
+        int dp = f_dp(10, 5, 3, 6);
         System.out.println(f);
         System.out.println(cache);
+        System.out.println(dp);
     }
 }
