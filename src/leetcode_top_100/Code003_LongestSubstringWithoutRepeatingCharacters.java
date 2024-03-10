4b132e2c6f84f35bf9c33f4d372997492c45d61e
Jiajun-Peng <pengjiajun566@gmail.com>
2024-03-10 18:43:36 +0800

    无重复字符串的最长子串

---
+++ src/leetcode_top_100/Code003_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,40 @@
+package leetcode_top_100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+[3. 无重复字符的最长子串](https://leetcode.cn/problems/longest-substring-without-repeating-characters/)
+ */
+public class Code003_LongestSubstringWithoutRepeatingCharacters {
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        char[] charArray = s.toCharArray();
+        Map<Integer, Integer> indexToLeftmostIndex = new HashMap<>(); // 用于保存以index结尾的子串最左的起始位置
+        indexToLeftmostIndex.put(0, 0);
+
+        int maxLength = 1; // 全局最大长度，注意，最小的长度至少为1
+        for (int i = 1; i < charArray.length; i++) {
+            char c = charArray[i];
+            int curLeftmostStartIndexWhenEqual = i; // 初始时，都是以自身为起始位置
+            for (int j = i - 1; j >= 0; j--) { // 向左遍历，直到等于当前i位置的位置
+                if (charArray[j] == c) break;
+                curLeftmostStartIndexWhenEqual = j; // 记录左边第一个等于当前位置的值
+            }
+            int preLeftmostStartIndex = indexToLeftmostIndex.get(i - 1); // i - 1位置最左的起始位置
+            // i位置最左的起始位置不会比i-1位置的最左起始位置更靠左
+            int curLeftmostStartIndex = Math.max(curLeftmostStartIndexWhenEqual, preLeftmostStartIndex); // 靠右的位置更大
+            maxLength = Math.max(maxLength, i - curLeftmostStartIndex + 1);
+            indexToLeftmostIndex.put(i, curLeftmostStartIndex); // 保存当前位置的最左起始位置
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int length = lengthOfLongestSubstring("abcabcbb");
+        System.out.println(length);
+    }
+}
