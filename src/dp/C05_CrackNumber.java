5b493aed60c5e180322fa9c32d87744e6f1e31a8
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-27 11:15:11 +0800

    数字转字符串，修改为动态规划

--- src/dp/C05_CrackNumber.java
+++ src/dp/C05_CrackNumber.java
@@ -33,8 +33,36 @@ public class C05_CrackNumber {
         return single + dual;
     }
 
+    public static int crackNumberWithDp(int ciphertext) {
+        if (ciphertext < 0) return 0;
+        char[] charArray = String.valueOf(ciphertext).toCharArray();
+        int length = charArray.length;
+        int[] dp = new int[length + 1]; // 只与遍历位置单个变量有关
+
+        dp[length] = 1; // 根据递归的终止条件，设置dp表
+
+        // 从右往左填充，也就是从位数少的情况逐渐推算出位数多的情况
+        for (int index = length - 1; index >= 0; index--) {
+            int single = dp[index + 1]; // 将递归调用修改为从dp表中获取结果
+            int dual = 0;
+            if ((index + 1 < charArray.length)) { // 以当前位置为起始，后面至少还有一位，可以与当前位置组成双数
+                int tensPlace = charArray[index] - '0'; // 0~9 的数字字符在 ascii 码表的值是 48 ~ 57
+                int unitsPlace = charArray[index + 1] - '0';
+                if (tensPlace > 0 && tensPlace <= 2) { // 双数不能以 0 开始
+                    int num = tensPlace * 10 + unitsPlace;
+                    if (num <= 25) { // 字母的范围是 0 ~ 25
+                        dual = dp[index + 2]; // 将递归调用修改为从dp表中获取结果
+                    }
+                }
+            }
+            dp[index] = single + dual; // 将递归中的返回语句修改为对dp表的赋值语句
+        }
+        return dp[0];
+    }
+
     public static void main(String[] args) {
-        int num = 216612;
-        System.out.println(crackNumber(num));
+        int ciphertext = 216612;
+        System.out.println(crackNumber(ciphertext));
+        System.out.println(crackNumberWithDp(ciphertext));
     }
 }
