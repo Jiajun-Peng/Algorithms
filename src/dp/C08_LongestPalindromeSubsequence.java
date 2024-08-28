6f4469675dc7e9128c1f301eba2a78662e0d294d
Jiajun-Peng <pengjiajun566@gmail.com>
2024-08-28 15:34:42 +0800

    最长回文子序列，添加链接

--- src/dp/C08_LongestPalindromeSubsequence.java
+++ src/dp/C08_LongestPalindromeSubsequence.java
@@ -1,9 +1,7 @@
 package dp;
 
-import java.lang.ref.Cleaner;
-
 /*
-
+516. 最长回文子序列：https://leetcode.cn/problems/longest-palindromic-subsequence
  */
 public class C08_LongestPalindromeSubsequence {
 
